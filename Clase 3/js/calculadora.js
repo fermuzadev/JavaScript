c142540ee3e7a49df7267b831a0d0851f58ab79b
Fernando Muzaber <fmuzaber@economicas.uba.ar>
2023-03-30 13:31:35 -0300

    JavaScriptv2

--- Clase 3/js/calculadora.js
+++ Clase 3/js/calculadora.js
@@ -6,17 +6,18 @@ let peso = parseFloat(prompt("Ingrese su peso"));
 //Proceso de Datos
 
 let imc = peso / (altura * altura); 
-let res = '';
+let res;
 //Validamos el IMC
 if (imc < 18.5) {
-    let res = " Peso inferior al normal";
+   res = " Peso inferior al normal";
  } else if (imc >= 18.5 && imc <= 24.9) {
-    let res = 'Normal';
+    res = 'Normal';
  } else if (imc >= 25 && imc <= 29.9) {
-    let res = "Peso superior al normal";
+    res = "Peso superior al normal";
  } else {
-    let res = 'Obesidad'; 
+   res = 'Obesidad'; 
  }
 //Salida
 alert("Tu IMC es : " + imc + "%");
-alert("Tu condicion de salud es:" +res);  ///ver el codigo de los archivos que suba al drive 
+alert("Tu condicion de salud es: " + res); 
+console.log("Tu condicion de salud es: " + res);
