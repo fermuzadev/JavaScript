29c330bc30f0fba08ca4bf91948f3935bb436a83
Fernando Muzaber <fmuzaber@economicas.uba.ar>
2023-05-22 16:18:21 -0300

    proyectoFinalv2.2

--- js/app.js
+++ js/app.js
@@ -1,27 +1,22 @@
-//DOM
+//DOM HTML 
 const listaViajes = document.querySelector('#listaViajes')
-const btnVaciar = document.getElementById('btnvaciar');
-const boton1 = document.getElementById('boton1');
-const boton2 = document.getElementById('boton2');
-const boton3 = document.getElementById('boton3');
-const boton4 = document.getElementById('boton4');
-const boton5 = document.getElementById('boton5');
-const boton6 = document.getElementById('boton6');
 const input = document.getElementById('input');
-const btnFinalizar = document.getElementById('btnfinalizar')
 const tabla = document.getElementById('viajes');
+const btnVaciar = document.getElementById('btnvaciar');
+const btnFinalizar = document.getElementById('btnfinalizar')
 
 //Funciones
-document.addEventListener('DOMContentLoaded', () => {
-    traerViajes();
-    agregarViaje();
+//Defino flujo de ejecución con asincronía para que tenga todo mapeado antes de modificar los elementos
+async function flujo() {
+    await traerViajes();
+    cargarBotones();
     vaciarFunction();
-})
+    actualizarStorage();
+}
 
-let subtotal = function(precio, cantidad) {
-    return precio*cantidad;
-} 
+document.addEventListener('DOMContentLoaded', flujo);
 
+//Me traigo el JSON, y llamo a la función que me genera el html dinámico
 async function traerViajes() {
     viajes = await fetch('../model/data.json').then((response) => {
         if (response.ok) {
@@ -41,94 +36,29 @@ async function traerViajes() {
         })
     })
     mapearViajes();
-    
 }
+
+//Genero mi HTML dinámico
 function mapearViajes() {
     let contador = 1;
-    let fila = document.createElement('div');
-    fila.classList.add('cards');
     viajes.forEach((viaje)=> {
-        if (contador <= 3) {
-            fila.innerHTML += `
+        if (contador < 11) {
+            let card = document.createElement('div');
+            card.classList.add('cards');
+            card.innerHTML += `
             <div class="cards">
                 <img src=${viaje.imagen} class="fluid imagen" alt="${viaje.destino}">
                 <button class="btn-info btn" id="boton${viaje.id}" type="button">Agregar</button>
             </div>
             `;
-
+            listaViajes.appendChild(card);
             contador++;
-        } else {
-            listaViajes.appendChild(fila);
-            fila = document.createElement('div');
-            fila.classList.add('cards');
-            contador = 1;
-        }
+        } 
     });
-    listaViajes.appendChild(fila);
+    mostrarViajes();
 }
 
-function agregarViaje() {
-    boton1.addEventListener('click', () => {
-        viajes.forEach((viaje) => {
-            if (viaje.id === 1) {
-                viaje.cantidad > 0 ? viaje.cantidad++ : viaje.cantidad = 1 
-            }
-        })
-    mostrarViajes() 
-    })
-    boton2.addEventListener('click', () => {
-        viajes.forEach((viaje) => {
-            if (viaje.id === 2) {
-                viaje.cantidad > 0 ? viaje.cantidad++ : viaje.cantidad = 1
-            }
-        })
-    mostrarViajes()
-    })
-    boton3.addEventListener('click', () => {
-        viajes.forEach((viaje) => {
-            if (viaje.id === 3) {
-                viaje.cantidad > 0 ? viaje.cantidad++ : viaje.cantidad = 1
-            }
-        })
-    mostrarViajes()
-    })
-    boton4.addEventListener('click', () => {
-        viajes.forEach((viaje) => {
-            if (viaje.id ===4) {
-                viaje.cantidad > 0 ? viaje.cantidad++ : viaje.cantidad = 1
-            }
-        })
-    mostrarViajes()
-    })
-    boton5.addEventListener('click', () => {
-        viajes.forEach((viaje) => {
-            if (viaje.id === 5) {
-                viaje.cantidad > 0 ? viaje.cantidad++ : viaje.cantidad = 1
-            }
-        })
-    mostrarViajes()
-    })
-    boton6.addEventListener('click', () => {
-        viajes.forEach((viaje) => {
-            if (viaje.id ===6 ) {
-                viaje.cantidad > 0 ? viaje.cantidad++ : viaje.cantidad = 1
-            }
-        })
-    mostrarViajes()
-    })
-    traerViajes();
-}
-
-btnFinalizar.addEventListener('click', () => {
-    Swal.fire({
-        title: 'En desarrollo',
-        text: 'Estamos trabajando en esto',
-        imageUrl: '../images/finalizar.jpg',
-        imageWidth: 300,
-        imageHeight: 300,
-        imageAlt: 'btn--finalizar--img',
-    })
-})
+// Crea la tabla generada por el usuario de acuerdo a los botones
 function mostrarViajes() {
     tabla.innerHTML = "";
     viajes.forEach((viaje) => {
@@ -154,7 +84,143 @@ function mostrarViajes() {
     tabla.appendChild(tr);
     actualizarStorage();
 }
+//Actualizo el localStorage con cada cambio implementado
+function actualizarStorage() {
+    localStorage.setItem('viajes', JSON.stringify(viajes))
+    viajes = JSON.parse(localStorage.getItem('viajes'))
+    Swal.fire({
+        title: 'LocalStorage actualizado',
+        text: 'El LS esta sincronizado',
+        icon: 'success',
+        confirmButtonText: 'Bien'
+    })
+    viajes.forEach((viaje)=> {
+        if (viaje.cantidad > 0) {
+        Toastify({
+            duration: 10000,
+            gravity: 'top',
+            oldestFirst: false,
+            close: true,
+            text:` Destino:  ${viaje.destino} Precio: ${viaje.precio} Cantidad: ${viaje.cantidad} Total:  ${subtotal(viaje.precio,viaje.cantidad)})`,
+            className: "info",
+            style: {
+                backgroundColor: "light-blue",
+            }
+        }).showToast();
+        }})
+    }
 
+function cargarBotones() {
+const boton1 = document.getElementById('boton1');
+const boton2 = document.getElementById('boton2');
+const boton3 = document.getElementById('boton3');
+const boton4 = document.getElementById('boton4');
+const boton5 = document.getElementById('boton5');
+const boton6 = document.getElementById('boton6');
+const boton7 = document.getElementById('boton7');
+const boton8 = document.getElementById('boton8');
+const boton9 = document.getElementById('boton9');
+const boton10 = document.getElementById('boton10');
+boton1.addEventListener('click', () => {
+    viajes.forEach((viaje) => {
+        if (viaje.id === 1) {
+            viaje.cantidad > 0 ? viaje.cantidad++ : viaje.cantidad = 1 
+        }
+    })
+    mostrarViajes();
+})
+boton2.addEventListener('click', () => {
+    viajes.forEach((viaje) => {
+        if (viaje.id === 2) {
+            viaje.cantidad > 0 ? viaje.cantidad++ : viaje.cantidad = 1
+        }
+    })
+    mostrarViajes();
+})
+boton3.addEventListener('click', () => {
+    viajes.forEach((viaje) => {
+        if (viaje.id === 3) {
+            viaje.cantidad > 0 ? viaje.cantidad++ : viaje.cantidad = 1
+        }
+    })
+    mostrarViajes();
+})
+boton4.addEventListener('click', () => {
+    viajes.forEach((viaje) => {
+        if (viaje.id ===4) {
+            viaje.cantidad > 0 ? viaje.cantidad++ : viaje.cantidad = 1
+        }
+    })
+    mostrarViajes();
+})
+boton5.addEventListener('click', () => {
+    viajes.forEach((viaje) => {
+        if (viaje.id === 5) {
+            viaje.cantidad > 0 ? viaje.cantidad++ : viaje.cantidad = 1
+        }
+    })
+    mostrarViajes();
+})
+boton6.addEventListener('click', () => {
+    viajes.forEach((viaje) => {
+        if (viaje.id ===6 ) {
+            viaje.cantidad > 0 ? viaje.cantidad++ : viaje.cantidad = 1
+        }
+    })
+    mostrarViajes();
+})
+boton7.addEventListener('click', () => {
+    viajes.forEach((viaje) => {
+        if (viaje.id ===7 ) {
+            viaje.cantidad > 0 ? viaje.cantidad++ : viaje.cantidad = 1
+        }
+    })
+    mostrarViajes();
+})
+boton8.addEventListener('click', () => {
+    viajes.forEach((viaje) => {
+        if (viaje.id ===8 ) {
+            viaje.cantidad > 0 ? viaje.cantidad++ : viaje.cantidad = 1
+        }
+    })
+    mostrarViajes();
+})
+boton9.addEventListener('click', () => {
+    viajes.forEach((viaje) => {
+        if (viaje.id ===9 ) {
+            viaje.cantidad > 0 ? viaje.cantidad++ : viaje.cantidad = 1
+        }
+    })
+    mostrarViajes(); 
+})
+boton10.addEventListener('click', () => {
+    viajes.forEach((viaje) => {
+        if (viaje.id ===10 ) {
+            viaje.cantidad > 0 ? viaje.cantidad++ : viaje.cantidad = 1
+        }
+    })
+    mostrarViajes();
+})
+}
+
+//Calculo los totales
+let subtotal = function(precio, cantidad) {
+    return precio*cantidad;
+} 
+
+//Botón para modificar luego en el caso de ir a pagar
+btnFinalizar.addEventListener('click', () => {
+    Swal.fire({
+        title: 'En desarrollo',
+        text: 'Estamos trabajando en esto',
+        imageUrl: '../images/finalizar.jpg',
+        imageWidth: 300,
+        imageHeight: 300,
+        imageAlt: 'btn--finalizar--img',
+    })
+})
+
+//Botón que vacía y actualiza el Storage con un swal 
 function vaciarFunction() {
     btnVaciar.addEventListener('click', () => {
         Swal.fire({
@@ -180,31 +246,5 @@ function vaciarFunction() {
         })
         })    
 }
-
-
-function actualizarStorage() {
-    localStorage.setItem('viajes', JSON.stringify(viajes))
-    viajes = JSON.parse(localStorage.getItem('viajes'))
-    Swal.fire({
-        title: 'LocalStorage actualizado',
-        text: 'El LS esta sincronizado',
-        icon: 'success',
-        confirmButtonText: 'Bien'
-    })
-    viajes.forEach((viaje)=> {
-        if (viaje.cantidad > 0) {
-    Toastify({
-        duration: 10000,
-        gravity: 'top',
-        oldestFirst: false,
-        close: true,
-        text:` Destino:  ${viaje.destino} Precio: ${viaje.precio} Cantidad: ${viaje.cantidad} Total:  ${subtotal(viaje.precio,viaje.cantidad)})`,
-        className: "info",
-        style: {
-            backgroundColor: "light-blue",
-        }
-        }).showToast();
-    }})
-    }
 
 
